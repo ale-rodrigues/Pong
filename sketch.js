716aa3834ffa25e3dbe7e4f310d7fc87903e0fa8
ale-rodrigues <alessandro.rodrigues@terra.com.br>
2023-08-04 16:49:25 -0300

    Ajustes criação da bola

--- sketch.js
+++ sketch.js
@@ -1,37 +1,60 @@
-//posição da bola
-let x = 200;
-let y = 200;
-//raio da bola
-const r = 25;
+//classe bola
+class Bola
+{
+    //construtor
+    constructor()
+    {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.r = 25;
+        this.vx = Math.floor(Math.random() * 10) - 5;
+        this.vy = Math.floor(Math.random() * 10) - 5;
+    }
+    
+    //desenha a bola
+    desenha()
+    {
+        fill(color(255, 0, 0));
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    }
+    
+    //atualiza a bola
+    atualiza()
+    {
+        this.x += this.vx;
+        this.y += this.vy;
+    
+        //se tocar na borda horizontal, reserta no meio da tela
+        if (this.x < this.r || this.x > width - this.r) {
+            this.reset();
+        }    
 
-//velocidade da bola aleatória
-var vx = Math.floor(Math.random() * 10) - 5;
-var vy = Math.floor(Math.random() * 10) - 5;
-
-//função setup do p5js
-function setup() {
-    createCanvas(800, 400);
-    background(0);
+        //idem para boarda vertical
+        if (this.y < this.r || this.y > height - this.r) {
+            this.vy *= -1;
+        }
+    }
+    
+    //reseta a bola
+    reset()
+    {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.vx = Math.floor(Math.random() * 10) - 5;
+        this.vy = Math.floor(Math.random() * 10) - 5;
+    }
 }
 
-//função draw do p5js
-function draw()
-{
-    //desenha um circulo   
-    background(0);
-    fill(255);
-    ellipse(x, y, r*2, r*2);
-    x += vx;
-    y += vy;
-    
-    //se tocar na borda horizontal, reserta no meio da tela
-    if (x < r || x > width - r) {
-        x = width / 2;
-        y = height / 2;
-    }    
+let bola;
 
-    //idem para boarda vertical
-    if (y < r || y > height - r) {
-        vy *= -1;
-    }
+function setup()
+{
+    createCanvas(800, 400);
+    bola = new Bola();
+}
+
+function draw(){
+    background(color(0, 0, 0));
+    bola.atualiza();
+    bola.desenha();
 }
